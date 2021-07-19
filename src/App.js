513f8cd84df083c3fdd285300f2a6976704f4f43
Chandrasekar <chandrasekar.p@techmango.net>
2021-07-19 19:42:13 +0530

    created widget app with functional component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Accordion from './components/Accordion';
+import Search from "./components/Search";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end javascript library'
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a fav library among engineers'
+  },
+  {
+    title: 'Lorem Ipsum',
+    content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s'
+  }
+];
+
+export default () => {
+  return(
+    <div>
+      <br />
+      {/*<Accordion items={items} />*/}
+      <div className={'ui container'}>
+        <Search />
+      </div>
     </div>
   );
 }
-
-export default App;
